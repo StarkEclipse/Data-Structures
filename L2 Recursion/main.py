d0f9afd18a5642c59c094236c9992f3a47c24e0d
StarkEclipse <nore_nate@icloud.com>
2026-08-11 19:54:14 +0200

    Add files via upload

---
+++ L2 Recursion/main.py
@@ -0,0 +1,44 @@
+# def countdown(n):
+#     if n == 0:
+#         print("blastoff")
+#         return
+#     print(n)    
+#     countdown(n - 1)
+# countdown(5)
+
+# def countup(n):
+#     if n == 0:
+#         return
+#     countup(n - 1)
+#     print(n)
+# countup(5)
+# 
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     return n * factorial(n - 1)
+# print(factorial(5))
+
+# def fibonacci(n):
+#     if n == 1:
+#         return 1
+#     if n == 0:
+#         return 0
+#     return fibonacci(n - 1) + fibonacci(n - 2)
+# print(fibonacci(6))
+
+# def launch(n):
+#     if n == 0:
+#         print("go")
+#         return
+#     print("T-minus", n)
+#     launch(n - 1)
+# launch(5)
+
+def launch(n):
+    if n == 0:
+        print("go")
+        return
+    print("T-minus", n)
+    launch(n - 2)
+launch(10)
