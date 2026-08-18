754b29d920815a56560d2f721cbe7b4d3768345d
StarkEclipse <nore_nate@icloud.com>
2026-08-18 19:53:37 +0200

    Add files via upload

---
+++ L3 Searching algorithm/main.py
@@ -0,0 +1,13 @@
+# numbers = [5, 12, 8, 20, 3]
+# numbers = [10, 25, 7, 42, 18]
+
+numbers = [4, 9, 2, 7, 15, 6]
+
+target = 15
+for number in numbers:
+    if target in numbers:
+        print(f"{target} is in the list")
+        break
+
+
+        
